c988d279e0be9ba4fde040c42a990ce4fcf2ff48
BuschMath <busch@iowacentral.edu>
2023-04-27 13:46:38 -0500

    Update comments

--- PaymentProcessor.java
+++ PaymentProcessor.java
@@ -1,3 +1,19 @@
+/* Suppose you have an existing class PaymentGateway that provides a 
+    method payWithCreditCard() to process credit card payments.
+
+    Now, you want to integrate a new payment service called 
+    PaypalPaymentService, but it has a different interface that expects 
+    an email and a password instead of a credit card number. You can 
+    create an adapter class PaypalAdapter to convert the interface of 
+    PaypalPaymentService to match the PaymentGateway interface.
+
+    Now you can use the PaypalAdapter class to process payments through 
+    PaypalPaymentService as if it were a credit card payment. This way, 
+    the PaypalPaymentService can be used seamlessly with the PaymentGateway 
+    interface, thanks to the PaypalAdapter class that adapts the interface 
+    of the PaypalPaymentService to match the PaymentGateway interface.
+*/
+
 public class PaymentProcessor {
     public static void main(String[] args) {
         PaymentGateway paymentGateway = new PaymentGateway();
